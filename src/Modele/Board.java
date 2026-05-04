3dc001836c60a1254baf4e7d10cce31bf8fbb0ad
radualdea <radumihaialdea@gmail.com>
2026-05-04 13:09:42 +0200

    getter pour numero des captures

--- src/Modele/Board.java
+++ src/Modele/Board.java
@@ -142,6 +142,17 @@ public class Board extends Observable {
     // Accesseurs de l'état d'une cellule
     // ------------------------------------------
 
+    public int getCaptured(int player){
+        switch (player) {
+            case 0:
+                return capturedByP0;
+            case 1:
+                return capturedByP1;
+            default:
+                return -1;
+        }
+    }
+
     // Retourne true si aucun joueur n'occupe cette cellule
     public boolean isEmpty(Cell c) {
         Integer idx = cellToSpiral.get(c);
